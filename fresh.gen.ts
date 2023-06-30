3df5b3d04e3f21022072c89957a4a8e28502f4af
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-06-30 20:28:29 +0200

    chore: handle error

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,14 +2,16 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/api/init.ts";
-import * as $1 from "./routes/index.tsx";
+import * as $0 from "./routes/api/agent.ts";
+import * as $1 from "./routes/api/init.ts";
+import * as $2 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    "./routes/api/init.ts": $0,
-    "./routes/index.tsx": $1,
+    "./routes/api/agent.ts": $0,
+    "./routes/api/init.ts": $1,
+    "./routes/index.tsx": $2,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
--- routes/api/agent.ts
+++ routes/api/agent.ts
@@ -1,14 +1,10 @@
 import { HandlerContext, Status } from "$fresh/server.ts";
 
 const options = {
-  method: "POST",
   headers: {
     "Content-Type": "application/json",
+    Authorization: "Bearer INSERT_TOKEN_HERE",
   },
-  body: JSON.stringify({
-    symbol: "lemonfarmer",
-    faction: "COSMIC",
-  }),
 };
 
 // https://docs.spacetraders.io/quickstart/new-game
@@ -18,24 +14,14 @@ export const handler = async (
 ): Promise<Response> => {
   try {
     const response = await fetch(
-      "https://api.spacetraders.io/v2/register",
+      "https://api.spacetraders.io/v2/my/agent",
       options
     );
     if (response.status === 404) {
       return new Response("Init: Not Found", { status: Status.NotFound });
     }
-    if (response.status === 409) {
-      const data = (await response.json()) as {
-        error?: { data?: { agentSymbol?: string } };
-      };
-      return new Response(
-        `Agent ${data.error?.data?.agentSymbol} already exists`,
-        {
-          status: Status.Conflict,
-        }
-      );
-    }
 
+    // e.g. {"data":{"accountId":"cljivzexei109s60cdhaspy1u","symbol":"LEMONFARMER","headquarters":"X1-YU85-99640B","credits":150000,"startingFaction":"COSMIC"}}
     return new Response(response.body);
   } catch (err) {
     return new Response(err.message ?? "Unexpected error", {
