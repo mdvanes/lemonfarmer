5cc2ec30f73ab798bc261bd3f7ff13ff155a5b6d
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-07-07 14:33:25 +0200

    add Option

--- components/Fp.tsx
+++ components/Fp.tsx
@@ -1,4 +1,5 @@
 import { pipe } from "fp-ts/lib/function";
+import * as O from "fp-ts/Option";
 
 function add1(num: number): number {
   return num + 1;
@@ -15,6 +16,40 @@ function concat(
   return [a, transformer(a)];
 }
 
+const getPossiblyNullSystem = (): string | null => {
+  return "system-id";
+};
+
+interface FWaypoint {
+  type: string;
+}
+
+const getPossiblyNullWaypointTypes = (
+  system: string
+): Array<FWaypoint | null> => {
+  return [];
+};
+
+const fwaypoint = {
+  type: "planet",
+} as FWaypoint | null;
+
+// type Option<A> = None | Some<A>
+
 export const Fp = () => {
+  //   pipe(fwaypoint, ({ type }) => type);
+  const x = pipe(
+    fwaypoint,
+    O.fromNullable,
+    O.map(({ type }) => type)
+  );
+  console.log(x);
+  const y = pipe(
+    null,
+    O.fromNullable,
+    O.map(({ type }) => type)
+  );
+  console.log(y);
+
   return <div>fp {pipe(1, add1, multiply2)}</div>;
 };
