caa97cefd9d366ffe3947db35478ed700c9f00f7
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-07-10 13:34:11 +0200

    chore: generate types

--- spacetrader.types.ts
+++ spacetrader.types.ts
@@ -1,27 +1,42 @@
-export interface Agent {
-  accountId: string;
-  symbol: string;
-  headquarters: string; // e.g. "X1-YU85-99640B";
-  credits: number;
-  startingFaction: string; // should be union of "COSMIC"...;
-}
+import { paths, external } from "./generated/schema.d.ts";
 
-export interface MyAgentResponse {
-  data: Agent;
-}
+/*
+Codegen:
+npx openapi-typescript https://raw.githubusercontent.com/SpaceTradersAPI/api-docs/main/reference/SpaceTraders.json -o ./generated/schema.d.ts
+// Almost works:
+deno run https://esm.sh/openapi-typescript/bin/cli.js https://raw.githubusercontent.com/SpaceTradersAPI/api-docs/main/reference/SpaceTraders.json -o ./generated/schema.d.ts
+*/
 
-export interface Waypoint {
-  chart: any;
-  faction: any;
-  orbitals: any;
-  symbol: string;
-  systemSymbol: string;
-  traits: any[];
-  x: number;
-  y: number;
-  type: string; // e.g. PLANET should be union
-}
+// export interface Agent {
+//   accountId: string;
+//   symbol: string;
+//   headquarters: string; // e.g. "X1-YU85-99640B";
+//   credits: number;
+//   startingFaction: string; // should be union of "COSMIC"...;
+// }
+export type Agent = external["../models/Agent.json"];
 
-export interface WaypointsResponse {
-  data: Waypoint[];
-}
+// export interface MyAgentResponse {
+//   data: Agent;
+// }
+export type MyAgentResponse =
+  paths["/my/agent"]["get"]["responses"][200]["content"]["application/json"];
+
+// export interface Waypoint {
+//   chart: any;
+//   faction: any;
+//   orbitals: any;
+//   symbol: string;
+//   systemSymbol: string;
+//   traits: any[];
+//   x: number;
+//   y: number;
+//   type: string; // e.g. PLANET should be union
+// }
+export type Waypoint = external["../models/Waypoint.json"];
+
+// export interface WaypointsResponse {
+//   data: Waypoint[];
+// }
+export type WaypointsResponse =
+  paths["/systems/{systemSymbol}/waypoints"]["get"]["responses"][200]["content"]["application/json"];
