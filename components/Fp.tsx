81a3d28794e73e24bb4ae882275addba75ec9768
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-07-10 13:33:13 +0200

    chore: process a possibly null array with possibly null items

--- components/Fp.tsx
+++ components/Fp.tsx
@@ -1,5 +1,6 @@
-import { pipe } from "fp-ts/lib/function";
+import { flow, pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/Option";
+import * as A from "fp-ts/Array";
 
 function add1(num: number): number {
   return num + 1;
@@ -26,7 +27,7 @@ interface FWaypoint {
 
 const getPossiblyNullWaypointTypes = (
   system: string
-): Array<FWaypoint | null> => {
+): Array<FWaypoint | null> | null => {
   return [];
 };
 
@@ -44,12 +45,75 @@ export const Fp = () => {
     O.map(({ type }) => type)
   );
   console.log(x);
+
   const y = pipe(
     null,
     O.fromNullable,
     O.map(({ type }) => type)
   );
   console.log(y);
+
+  const head = (as: Array<FWaypoint>): O.Option<FWaypoint> =>
+    as.length === 0 ? O.none : O.some(as[0]);
+
+  const z = getPossiblyNullWaypointTypes("a");
+  //   const z1a = O.map(pipe(z, O.fromNullable));
+  const foo: Array<FWaypoint | null> = [{ type: "planet" }, null];
+  const z1 = pipe(
+    // z,
+    // O.fromNullable,
+    foo,
+    A.filterMap(O.fromNullable),
+    A.map(({ type }) => type)
+    // head,
+    // O.map((n) => n),
+    // A.map((n: O.Option<FWaypoint>) =>
+    //   flow(
+    //     n,
+    //     O.fromNullable,
+    //     O.map(({ type }) => type)
+    //   )
+    // )
+    // A.map((n) =>
+    //   O.chain(
+    //     flow(
+    //       n,
+    //       O.fromNullable,
+    //       O.map(({ type }) => type)
+    //     )
+    //   )
+    // )
+  );
+
+  /* 1 this works */
+  const someWaypointOrNulls: Array<FWaypoint | null> = [
+    { type: "planet" },
+    null,
+  ];
+  const waypointTypes = pipe(
+    someWaypointOrNulls,
+    A.filterMap(O.fromNullable),
+    A.map(({ type }) => type)
+  );
+  /* 1 end this works */
+
+  /* 2 this works */
+  //   const someWaypointOrNulls2: Array<FWaypoint | null> | null = null;
+  const waypointTypes2 = pipe(
+    // someWaypointOrNulls2,
+    getPossiblyNullWaypointTypes("a"),
+    O.fromNullable,
+    O.match(
+      () => [],
+      (x) => x
+    ),
+    A.filterMap(O.fromNullable),
+    A.map(({ type }) => type)
+  );
+  console.log(waypointTypes2);
+  /* 2 end this works */
+
+  /// TODO add promises
 
   return <div>fp {pipe(1, add1, multiply2)}</div>;
 };
